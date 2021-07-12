3167240cda7f89ce0513f43c9617c9b0b9147a33
wuliw-toh <39265385+wuliw-toh@users.noreply.github.com>
2021-07-12 12:13:59 +0300

    bilding_work

--- code_war_wuliw/mind.py
+++ code_war_wuliw/mind.py
@@ -1,3 +1,4 @@
+from model import *
 
 class Mind:
     #Временные тестовые переменные
@@ -19,44 +20,81 @@ class Mind:
         self.units = []
 
     def updata(self,pleer_wiew):
-        #определили ресус
-        for pl in pleer_wiew.players:
-            if pl.id == self.id:
-                self.resurse = pl.resource
+        self.event_cheak(pleer_wiew)
+        #self.debag_print()
 
-        #проверка порога ресурса
-        if self.resurse > 600:
-            self.a_lot_of_res = True
-        else:
-            self.a_lot_of_res = False
+        result = Action({})
+        kost_fix = 0
+        kost_war = True
 
-        #Нашли новых юнитов
-        old_ent, new_ent = self.chek_unit(pleer_wiew.entities)
+        for unit in self.units:
+            move_act = None
+            build_act = None
+            attack_act = AttackAction(None, AutoAttack(50, [EntityType.RESOURCE]))
+            fix_act = None
 
-        #Проверка на появление юнита
-        if new_ent:
-            self.new_unit = True
-            #распределение по спискам
-            self.to_list(new_ent)
-        else:
-            self.new_unit = False
 
-        #проверка на наличие еды
-        self.eat_work()
-        if self.max_eat == self.real_eat:
-            self.need_eat = True
-        else:
-            self.need_eat = False
+            if self.need_eat:
+                self.need_eat = False
+                if len(self.building) == 1:
+                    vek_stop = Vec2Int(2,1)
+                    vek_act = Vec2Int(2,2)
+                elif len(self.building) == 2:
+                    vek_stop = Vec2Int(5,1)
+                    vek_act = Vec2Int(5,2)
+                else:
+                    vek_stop = Vec2Int(8, 1)
+                    vek_act = Vec2Int(8, 2)
 
-        #проверка на наличие порушеннах зданий
-        for i in self.building:
-            properties = self.Entity_Properties[i.entity_type]
-            if properties.max_health > i.health:
-                self.need_fix = True
-            else:
-                self.need_fix = False
+                move_act = MoveAction(vek_stop,True,True)
+                build_act = BuildAction(EntityType.HOUSE,vek_act)
+                attack_act = None
 
-        self.debag_print()
+            if self.need_fix:
+                target = None
+                for i in self.building:
+                    properties = self.Entity_Properties[i.entity_type]
+                    if properties.max_health > i.health:
+                        target = i
+                        break
+                if kost_fix < 5:
+                    move_act = MoveAction(target.position,True,True)
+                    fix_act = RepairAction(target.id)
+                    attack_act = None
+                    kost_fix += 1
+
+            if self.resurse > 500:
+                if kost_war :
+                    kost_war = False
+                    move_act = MoveAction(Vec2Int(11,10), True, True)
+                    build_act = BuildAction(EntityType.RANGED_BASE, Vec2Int(11,11))
+                    attack_act = None
+
+
+
+            result.entity_actions[unit.id] = EntityAction(
+                move_action=move_act,
+                build_action=build_act,
+                attack_action=attack_act,
+                repair_action=fix_act
+            )
+
+        for house in self.building:
+            #Внимание работает только на главном здании.
+            #Просто по кд строит рабочих
+            build_act = BuildAction(EntityType.BUILDER_UNIT,
+                                    Vec2Int(house.position.x + 5,
+                                            house.position.y + 5 - 1))
+
+
+            result.entity_actions[house.id] = EntityAction(
+                move_action=None,
+                build_action=build_act,
+                attack_action=None,
+                repair_action=None
+            )
+
+        return result
 
     def chek_unit(self, ent_s):
         old_unit = []
@@ -88,6 +126,60 @@ class Mind:
 
         self.real_eat = len(self.units)
 
+    def event_cheak(self, pleer_wiew):
+        #определили ресус
+        for pl in pleer_wiew.players:
+            if pl.id == self.id:
+                self.resurse = pl.resource
+
+        #проверка порога ресурса
+        if self.resurse > 600:
+            self.a_lot_of_res = True
+        else:
+            self.a_lot_of_res = False
+
+
+        #Нашли новых юнитов
+        #old_ent, new_ent = self.chek_unit(pleer_wiew.entities)
+
+        #Проверка на появление юнита
+        #if new_ent:
+        #    self.new_unit = True
+        #    #распределение по спискам
+        #    self.to_list(new_ent)
+        #else:
+        #    self.new_unit = False
+
+        #проверка на наличие еды
+
+        my_point = []
+        self.building = []
+        self.units = []
+
+        for i in pleer_wiew.entities:
+            if i.player_id == self.id:
+                my_point.append(i)
+
+        self.to_list(my_point)
+
+
+        self.eat_work()
+        if self.max_eat == self.real_eat:
+            self.need_eat = True
+        else:
+            self.need_eat = False
+
+
+
+        #проверка на наличие порушеннах зданий
+        # НЕ ОБНОВЛЯЕТЬСЯ СПИСКИ!!!!
+        self.need_fix = False
+        for i in self.building:
+            properties = self.Entity_Properties[i.entity_type]
+            if properties.max_health > i.health:
+                self.need_fix = True
+
+
 
     def debag_print(self):
         print("\n")
@@ -97,3 +189,7 @@ class Mind:
         print("Макс еда - ", self.max_eat)
         print("Реал еда - ", self.real_eat)
         print("Надо чинить - ", self.need_fix)
+
+        #отладка постройки
+        if self.need_fix:
+            print(self.building[1])
--- my_strategy.py
+++ my_strategy.py
@@ -13,41 +13,8 @@ class MyStrategy:
         if self.first_config:
             self.mind.Entity_Properties = player_view.entity_properties
             self.first_config = False
-
-        self.mind.updata(player_view)
-
-        result = Action({})
-        my_id = player_view.my_id
-
-
-        for entity in player_view.entities:
-            if entity.player_id != my_id:
-                continue
-            properties = player_view.entity_properties[entity.entity_type]
-
-            move_action = None
-            build_action = None
-
-            if properties.can_move:
-                move_action = MoveAction(
-                    Vec2Int(player_view.map_size - 1,
-                            player_view.map_size - 1),
-                    True,
-                    True)
-
-            elif properties.build is not None:
-                build_action = BuildAction(
-                        EntityType.BUILDER_UNIT,
-                        Vec2Int(entity.position.x + properties.size, entity.position.y + properties.size - 1))
-
-            result.entity_actions[entity.id] = EntityAction(
-                move_action,
-                build_action,
-                AttackAction(None, AutoAttack(properties.sight_range, [EntityType.RESOURCE])),
-                None
-            )
-
-        return result
+        out = self.mind.updata(player_view)
+        return out
 
 
     def debug_update(self, player_view, debug_interface):
